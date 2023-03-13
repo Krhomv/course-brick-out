8d25a801e1ffcb3d579a6aa8a489284723f75f5a
Krhomv <krhom.v@gmail.com>
2023-03-13 21:02:43 +0100

    Initial Setup
    Main project and starter code.

---
+++ course-brick-out/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace course_brick_out
+{
+    internal class Game
+    {
+        public Game()
+        {
+            Console.WriteLine("Construct");
+        }
+
+        public void Init()
+        {
+            Console.WriteLine("Init");
+        }
+
+        // Returns whether the game loop should continue.
+        public bool Update()
+        {
+            Console.WriteLine("Update");
+
+            return !Keyboard.IsKeyDown(Key.Escape);
+        }
+
+        public void Render()
+        {
+            Console.WriteLine("Render");
+        }
+    }
+}
---
+++ course-brick-out/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace course_brick_out // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            game.Init();
+
+            var running = true;
+            var stopwatch = new Stopwatch();
+            double targetFrameTime = 1.0 / 60.0; // 60 FPS
+
+            while (running)
+            {
+                stopwatch.Start();
+                running = game.Update();
+                game.Render();
+                stopwatch.Stop();
+
+                if (stopwatch.Elapsed.TotalSeconds < targetFrameTime)
+                {
+                    var remainingSecondsToWait = targetFrameTime - stopwatch.Elapsed.TotalSeconds;
+                    Thread.Sleep(TimeSpan.FromSeconds(remainingSecondsToWait));
+                }
+                stopwatch.Reset();
+            }
+        }
+    }
+}
+
